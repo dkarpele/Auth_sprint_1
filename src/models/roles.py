653c5124e222beec7c96942132ade0d176ca236f
lubovSovina <sovinaliubov@yandex.ru>
2023-06-25 22:26:47 +0300

    Add unique for title

--- src/models/roles.py
+++ src/models/roles.py
@@ -12,7 +12,7 @@ class Role(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,
                 unique=True, nullable=False)
-    title = Column(String(255), nullable=False)
+    title = Column(String(255), unique=True, nullable=False)
     permissions = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
