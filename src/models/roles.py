8a85415e6a55bc7de811e2bde617789657c5d24d
lubovSovina <sovinaliubov@yandex.ru>
2023-06-27 14:02:53 +0300

    Create table users_roles

--- src/models/roles.py
+++ src/models/roles.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, String, Integer
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 
 from db.postgres import Base
@@ -22,3 +22,17 @@ class Role(Base):
 
     def __repr__(self) -> str:
         return f'<Role {self.title}>'
+
+
+class UserRole(Base):
+    __tablename__ = 'users_roles'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,
+                unique=True, nullable=False)
+    user_id = Column(UUID, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    role_id = Column(UUID, ForeignKey('roles.id', ondelete='CASCADE'), nullable=False)
+    user_role_idx = UniqueConstraint('user_id', 'role_id')
+
+    def __init__(self, user_id: UUID, role_id: UUID) -> None:
+        self.user_id = user_id
+        self.role_id = role_id
