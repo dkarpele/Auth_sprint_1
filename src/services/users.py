827dc669450bad65e14a51f0fad4ab1d30c58457
lubovSovina <sovinaliubov@yandex.ru>
2023-06-27 14:04:26 +0300

    Create method for check admin

--- src/services/users.py
+++ src/services/users.py
@@ -12,6 +12,7 @@ from services.token import verify_password, TokenData, \
 from services.exceptions import credentials_exception
 from db.postgres import get_session
 from models.entity import User
+from models.roles import UserRole, Role
 from schemas.entity import UserInDB
 
 
@@ -56,9 +57,38 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
 
 
 async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
+        current_user: Annotated[User, Depends(get_current_user)]
 ):
     if current_user.disabled:
         raise HTTPException(status_code=400,
                             detail="Inactive user")
     return current_user
+
+
+async def check_admin_user(token: Annotated[str, Depends(oauth2_scheme)],
+                           db: AsyncSession = Depends(get_session)):
+    try:
+        user_id, _ = await decode_token(token, SECRET_KEY)
+
+        roles_exists = await db.execute(
+            select(UserRole).
+            filter(UserRole.user_id == user_id)
+        )
+        all_roles = [role.role_id for role in roles_exists.scalars().all()]
+        permissions = 0
+        for r_id in all_roles:
+            response = await db.execute(select(Role).filter(Role.id == r_id))
+            role = response.scalars().all()[0]
+            if role.permissions > permissions:
+                permissions = role.permissions
+        admin = await db.execute(
+            select(Role).
+            filter(Role.title == 'admin')
+        )
+        if admin.scalars().all()[0].permissions <= permissions:
+            return True
+        raise HTTPException(status_code=403,
+                            detail="You don't have permission to access on this server.")
+    except IndexError:
+        raise HTTPException(status_code=404,
+                            detail="Role admin not found.")
